7bae258ec2fdf371be72dca566aef276ac03f016
Piga962 <cesarjrmecinas@gmail.com>
2024-04-16 20:40:49 -0600

    Primer Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {Routes, Route} from 'react-router-dom';
+import './App.css'
+import Register from './components/Register'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Register />} />
+    </Routes>
+  )
+}
+
+export default App;
---
+++ src/components/Register.jsx
@@ -0,0 +1,9 @@
+
+
+const Register = () => {
+    return (
+        <div className="text-xl">Login</div>
+    )
+}
+
+export default Register;
